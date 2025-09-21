1c1e126035429b6e9e81424bad768dfd656cd4a2
jhonatanrp05 <jpachecos@uninorte.edu.co>
2025-09-21 11:42:53 -0500

    reorder icon files and page icon changed

--- src/main.js
+++ src/main.js
@@ -1,19 +1,16 @@
 import "./style.css";
 
-const users = [
-  {
+const users = [{
     username: "admin",
     password: "admin",
-  },
-];
+}, ];
 
-const tasks = localStorage.getItem("tasksList")
-  ? JSON.parse(localStorage.getItem("tasksList"))
-  : [];
+const tasks = localStorage.getItem("tasksList") ?
+    JSON.parse(localStorage.getItem("tasksList")) : [];
 
-let idList = localStorage.getItem("idList")
-  ? JSON.parse(localStorage.getItem("idList"))
-  : 0;
+let idList = localStorage.getItem("idList") ?
+    JSON.parse(localStorage.getItem("idList")) :
+    0;
 
 const loginPage = /*html*/ `
 <div class="container">
@@ -45,161 +42,159 @@ const toDoListPage = /*html*/ `
 `;
 
 const isLoggedIn = () => {
-  const data = localStorage.getItem("loggedInUser");
-  if (!data) return false;
-  const loggedInUser = JSON.parse(data);
-  return (
-    loggedInUser[0].username === users[0].username &&
-    loggedInUser[0].password === users[0].password
-  );
+    const data = localStorage.getItem("loggedInUser");
+    if (!data) return false;
+    const loggedInUser = JSON.parse(data);
+    return (
+        loggedInUser[0].username === users[0].username &&
+        loggedInUser[0].password === users[0].password
+    );
 };
 
 const renderApp = () => {
-  if (isLoggedIn()) {
-    document.querySelector("#app").innerHTML = toDoListPage;
-    const logoutButton = document.getElementById("logoutButton");
-    if (logoutButton) {
-      logoutButton.addEventListener("click", function () {
-        localStorage.removeItem("loggedInUser");
-        renderApp();
-      });
-    }
-
-    const addTaskButton = document.getElementById("addTaskButton");
-    const taskInput = document.getElementById("taskInput");
-    const taskList = document.getElementById("taskList");
-
-    const renderTasks = () => {
-      taskList.innerHTML = "";
-      tasks.forEach((task, index) => {
-        const li = document.createElement("li");
-        if (task.done) {
-          li.className = "taskDone";
+    if (isLoggedIn()) {
+        document.querySelector("#app").innerHTML = toDoListPage;
+        const logoutButton = document.getElementById("logoutButton");
+        if (logoutButton) {
+            logoutButton.addEventListener("click", function() {
+                localStorage.removeItem("loggedInUser");
+                renderApp();
+            });
         }
-        li.textContent = task.text;
 
-        const checkIcon = document.createElement("img");
+        const addTaskButton = document.getElementById("addTaskButton");
+        const taskInput = document.getElementById("taskInput");
+        const taskList = document.getElementById("taskList");
 
-        checkIcon.alt = "CheckIcon";
-        if (task.done) {
-          checkIcon.className = "checkIconDone";
-          checkIcon.src = "./src/checkIconWhite.svg";
-        } else {
-          checkIcon.className = "checkIcon";
-          checkIcon.src = "./src/checkIcon.svg";
-        }
-        checkIcon.addEventListener("click", () => {
-          task.done = !task.done;
-          task.updatedAt = new Date().toLocaleString();
-          localStorage.setItem("tasksList", JSON.stringify(tasks));
-          renderTasks();
-        });
+        const renderTasks = () => {
+            taskList.innerHTML = "";
+            tasks.forEach((task, index) => {
+                const li = document.createElement("li");
+                if (task.done) {
+                    li.className = "taskDone";
+                }
+                li.textContent = task.text;
 
-        const trashIcon = document.createElement("img");
-        trashIcon.src = "./src/trashIcon.svg";
-        trashIcon.alt = "TrashIcon";
-        trashIcon.className = "trashIcon";
-        trashIcon.addEventListener("click", () => {
-          tasks.splice(index, 1);
-          localStorage.setItem("tasksList", JSON.stringify(tasks));
-          renderTasks();
-        });
+                const checkIcon = document.createElement("img");
 
-        const editIcon = document.createElement("img");
-        editIcon.src = "./src/editIcon.svg";
-        editIcon.alt = "EditIcon";
-        editIcon.className = "editIcon";
-        editIcon.addEventListener("click", () => {
-          const inputTextEdit = document.createElement("input");
-          inputTextEdit.type = "text";
-          inputTextEdit.value = task.text;
-          const closeIcon = document.createElement("img");
-          closeIcon.src = "./src/closeIcon.svg";
-          closeIcon.alt = "CloseIcon";
-          closeIcon.className = "closeIcon";
-          closeIcon.addEventListener("click", () => {
-            renderTasks();
-          });
-          li.innerHTML = "";
-          actionsButtonsContainer.innerHTML = "";
-          actionsButtonsContainer.appendChild(closeIcon);
-          li.appendChild(inputTextEdit);
-          li.appendChild(actionsButtonsContainer);
+                checkIcon.alt = "CheckIcon";
+                if (task.done) {
+                    checkIcon.className = "checkIconDone";
+                    checkIcon.src = "./src/icons/checkIconWhite.svg";
+                } else {
+                    checkIcon.className = "checkIcon";
+                    checkIcon.src = "./src/icons/checkIcon.svg";
+                }
+                checkIcon.addEventListener("click", () => {
+                    task.done = !task.done;
+                    task.updatedAt = new Date().toLocaleString();
+                    localStorage.setItem("tasksList", JSON.stringify(tasks));
+                    renderTasks();
+                });
 
-          inputTextEdit.focus();
+                const trashIcon = document.createElement("img");
+                trashIcon.src = "./src/icons/trashIcon.svg";
+                trashIcon.alt = "TrashIcon";
+                trashIcon.className = "trashIcon";
+                trashIcon.addEventListener("click", () => {
+                    tasks.splice(index, 1);
+                    localStorage.setItem("tasksList", JSON.stringify(tasks));
+                    renderTasks();
+                });
 
-          inputTextEdit.addEventListener("keydown", (e) => {
-            if (e.key === "Enter") {
-              const newText = inputTextEdit.value.trim();
-              if (newText) {
-                task.text = newText;
-                task.updatedAt = new Date().toLocaleString();
+                const editIcon = document.createElement("img");
+                editIcon.src = "./src/icons/editIcon.svg";
+                editIcon.alt = "EditIcon";
+                editIcon.className = "editIcon";
+                editIcon.addEventListener("click", () => {
+                    const inputTextEdit = document.createElement("input");
+                    inputTextEdit.type = "text";
+                    inputTextEdit.value = task.text;
+                    const closeIcon = document.createElement("img");
+                    closeIcon.src = "./src/icons/closeIcon.svg";
+                    closeIcon.alt = "CloseIcon";
+                    closeIcon.className = "closeIcon";
+                    closeIcon.addEventListener("click", () => {
+                        renderTasks();
+                    });
+                    li.innerHTML = "";
+                    actionsButtonsContainer.innerHTML = "";
+                    actionsButtonsContainer.appendChild(closeIcon);
+                    li.appendChild(inputTextEdit);
+                    li.appendChild(actionsButtonsContainer);
+
+                    inputTextEdit.focus();
+
+                    inputTextEdit.addEventListener("keydown", (e) => {
+                        if (e.key === "Enter") {
+                            const newText = inputTextEdit.value.trim();
+                            if (newText) {
+                                task.text = newText;
+                                task.updatedAt = new Date().toLocaleString();
+                                localStorage.setItem("tasksList", JSON.stringify(tasks));
+                                renderTasks();
+                            }
+                        }
+                    });
+                });
+
+                const actionsButtonsContainer = document.createElement("div");
+                actionsButtonsContainer.className = "actionsButtonsContainer";
+                li.appendChild(actionsButtonsContainer);
+                actionsButtonsContainer.appendChild(checkIcon);
+                actionsButtonsContainer.appendChild(editIcon);
+                actionsButtonsContainer.appendChild(trashIcon);
+                taskList.appendChild(li);
+            });
+        };
+
+        const addTask = () => {
+            const text = taskInput.value.trim();
+            if (text) {
+                const now = new Date();
+                const newTask = {
+                    id: idList + 1,
+                    text,
+                    done: false,
+                    createdAt: now.toLocaleString(),
+                    updatedAt: now.toLocaleString(),
+                };
+                idList += 1;
+                tasks.push(newTask);
+                taskInput.value = "";
                 localStorage.setItem("tasksList", JSON.stringify(tasks));
                 renderTasks();
-              }
             }
-          });
-        });
-
-        const actionsButtonsContainer = document.createElement("div");
-        actionsButtonsContainer.className = "actionsButtonsContainer";
-        li.appendChild(actionsButtonsContainer);
-        actionsButtonsContainer.appendChild(checkIcon);
-        actionsButtonsContainer.appendChild(editIcon);
-        actionsButtonsContainer.appendChild(trashIcon);
-        taskList.appendChild(li);
-      });
-    };
-
-    const addTask = () => {
-      const text = taskInput.value.trim();
-      if (text) {
-        const now = new Date();
-        const newTask = {
-          id: idList + 1,
-          text,
-          done: false,
-          createdAt: now.toLocaleString(),
-          updatedAt: now.toLocaleString(),
         };
-        idList += 1;
-        tasks.push(newTask);
-        taskInput.value = "";
-        localStorage.setItem("tasksList", JSON.stringify(tasks));
+
+        addTaskButton.addEventListener("click", addTask);
         renderTasks();
-      }
-    };
+    } else {
+        document.querySelector("#app").innerHTML = loginPage;
 
-    addTaskButton.addEventListener("click", addTask);
-    renderTasks();
-  } else {
-    document.querySelector("#app").innerHTML = loginPage;
+        const loginForm = document.querySelector(".loginForm");
+        if (loginForm) {
+            loginForm.addEventListener("submit", function(e) {
+                e.preventDefault();
+                const username = document.getElementById("inputUsername").value;
+                const password = document.getElementById("inputPassword").value;
 
-    const loginForm = document.querySelector(".loginForm");
-    if (loginForm) {
-      loginForm.addEventListener("submit", function (e) {
-        e.preventDefault();
-        const username = document.getElementById("inputUsername").value;
-        const password = document.getElementById("inputPassword").value;
-
-        if (username === users[0].username && password === users[0].password) {
-          localStorage.setItem(
-            "loggedInUser",
-            JSON.stringify([
-              {
-                username,
-                password,
-              },
-            ])
-          );
-          renderApp();
-        } else {
-          document.getElementById("errorMessage").textContent =
-            "Invalid credentials";
+                if (username === users[0].username && password === users[0].password) {
+                    localStorage.setItem(
+                        "loggedInUser",
+                        JSON.stringify([{
+                            username,
+                            password,
+                        }, ])
+                    );
+                    renderApp();
+                } else {
+                    document.getElementById("errorMessage").textContent =
+                        "Invalid credentials";
+                }
+            });
         }
-      });
     }
-  }
 };
 
 renderApp();
