c984e6b1478920e0aa00b8751af5566c26bba24c
jhonatanrp05 <jpachecos@uninorte.edu.co>
2025-09-20 21:36:23 -0500

    update login page to include error message handling and user authentication

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ const loginPage = /*html*/ `
 <div class="container">
 
     <div class="loginContainer">
-        <h2>Login</h2>
+        <h2>TO DO LIST</h2>
+        <h3 id="errorMessage"></h3>
         <form class="loginForm">
             <input type="text" id="inputUsername" name="username" required placeholder="Username" />
             <input type="password" id="inputPassword" name="password" required placeholder="Password" />
@@ -21,9 +22,57 @@ const toDoListPage = /*html*/ `
         <button id="counter" type="button">count is 0</button>
     </div>
 </div>
-
-
-
 `;
 
-document.querySelector("#app").innerHTML = loginPage;
+const users = [
+  {
+    username: "admin",
+    password: "admin",
+  },
+];
+
+const isLoggedIn = () => {
+  const data = localStorage.getItem("loggedInUser");
+  if (!data) return false;
+  const loggedInUser = JSON.parse(data);
+  return (
+    loggedInUser[0].username === users[0].username &&
+    loggedInUser[0].password === users[0].password
+  );
+};
+
+const renderApp = () => {
+  if (isLoggedIn()) {
+    document.querySelector("#app").innerHTML = toDoListPage;
+  } else {
+    document.querySelector("#app").innerHTML = loginPage;
+
+    const loginForm = document.querySelector(".loginForm");
+    if (loginForm) {
+      loginForm.addEventListener("submit", function (e) {
+        console.log(e);
+        e.preventDefault();
+        const username = document.getElementById("inputUsername").value;
+        const password = document.getElementById("inputPassword").value;
+
+        if (username === users[0].username && password === users[0].password) {
+          localStorage.setItem(
+            "loggedInUser",
+            JSON.stringify([
+              {
+                username,
+                password,
+              },
+            ])
+          );
+          renderApp();
+        } else {
+          document.getElementById("errorMessage").textContent =
+            "Invalid credentials";
+        }
+      });
+    }
+  }
+};
+
+renderApp();
